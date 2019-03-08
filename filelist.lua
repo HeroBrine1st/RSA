4bfe97cd1fbffb96955fa72688774b68a8c67c59
HeroBrine1st <herobrine1.videos@gmail.com>
2019-03-08 18:02:49 +0300

    Create filelist.lua

---
+++ filelist.lua
@@ -0,0 +1,6 @@
+{
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/text_support.lua",path="/lib/RSA/text_support.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSAB.lua",path="/lib/RSA/RSAB.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/init.lua",path="/lib/RSA/init.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/metaint.lua"},
+}
