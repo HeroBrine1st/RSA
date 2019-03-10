ceb1b829c265378b6bb94994f775d971292b0198
HeroBrine1st <herobrine1.videos@gmail.com>
2019-03-10 22:25:18 +0300

    Update filelist.lua

--- filelist.lua
+++ filelist.lua
@@ -2,5 +2,5 @@
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/text_support.lua",path="/lib/RSA/text_support.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSAB.lua",path="/lib/RSA/RSAB.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/init.lua",path="/lib/RSA/init.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/metaint.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/lib/metaint.lua"},
 }
