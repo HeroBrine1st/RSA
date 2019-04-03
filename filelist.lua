0a13849ca8e6e47e91251808162183b202756817
HeroBrine1st <herobrine1.videos@gmail.com>
2019-04-03 18:37:33 +0300

    Update filelist.lua

--- filelist.lua
+++ filelist.lua
@@ -4,5 +4,5 @@
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/init.lua",path="/lib/RSA/init.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/lib/metaint.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSA_math.lua",path="/lib/RSA/RSA_math.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSA/MillerRabinTest.lua","/lib/RSA/MillerRabinTest.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/MillerRabinTest.lua","/lib/RSA/MillerRabinTest.lua"},
 }
