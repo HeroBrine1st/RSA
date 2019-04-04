4adafaedc6a83459888f34faa07d0017f99e74a7
HeroBrine1st <herobrine1.videos@gmail.com>
2019-04-04 20:24:28 +0300

    Update filelist.lua

--- filelist.lua
+++ filelist.lua
@@ -1,6 +1,6 @@
 {
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/text_support.lua",path="/lib/RSA/text_support.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Basic.lua",path="/lib/RSA/RSAB.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Basic.lua",path="/lib/RSA/Basic.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/init.lua",path="/lib/RSA/init.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/lib/metaint.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Math/init.lua",path="/lib/RSA/Math/init.lua"},
