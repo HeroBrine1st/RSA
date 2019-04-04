7c6601325ff1d86143d183b8c32e8f29b2613a69
HeroBrine1st <herobrine1.videos@gmail.com>
2019-04-04 20:23:09 +0300

    Update filelist.lua

--- filelist.lua
+++ filelist.lua
@@ -1,8 +1,9 @@
 {
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/text_support.lua",path="/lib/RSA/text_support.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSAB.lua",path="/lib/RSA/RSAB.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Basic.lua",path="/lib/RSA/RSAB.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/init.lua",path="/lib/RSA/init.lua"},
     {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/metaint.lua",path="/lib/metaint.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/RSA_math.lua",path="/lib/RSA/RSA_math.lua"},
-    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/MillerRabinTest.lua",path="/lib/RSA/MillerRabinTest.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Math/init.lua",path="/lib/RSA/Math/init.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Math/MillerRabinTest.lua",path="/lib/RSA/Math/MillerRabinTest.lua"},
+    {url="https://raw.githubusercontent.com/HeroBrine1st/RSA/master/RSA/Math/RandomNum.lua",path="/lib/RSA/Math/RandomNum.lua"}
 }
